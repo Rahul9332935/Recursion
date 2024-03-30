3bdc74e4ab7ed798d58c36d5347dbad842620384
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2024-03-30 12:35:53 +0530

    Q2

--- recursion/src/com/recursion/Main.java
+++ recursion/src/com/recursion/Main.java
@@ -3,7 +3,10 @@ package com.recursion;
 public class Main {
 	public static void main(String[] args) {
 		
-		printNameNTimes("Rahul", 4);
+//		printNameNTimes("Rahul", 4);
+//		print1ToN(9);
+		int a= sum(10);
+		System.out.println(a);
 		
 	}
 	public static void printNameNTimes(String name,int n) {
@@ -13,4 +16,22 @@ public class Main {
 		System.out.println(name);
 		printNameNTimes(name, n-1);
 	}
+	
+	public static void print1ToN(int n) {
+		if(n==0) {
+			return;
+		}
+		print1ToN(n-1);
+		System.out.println(n);
+	}
+	
+	public static int sum(int n) {
+		if(n==0) {
+			return 0;
+		}
+		return n+ sum(n-1);
+	}
+	
+	
+	
 }
