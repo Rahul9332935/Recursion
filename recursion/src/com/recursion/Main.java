9b11773d0ba1573b415a18f3e9a9665e920f479b
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2024-03-30 13:11:29 +0530

    Q3

--- recursion/src/com/recursion/Main.java
+++ recursion/src/com/recursion/Main.java
@@ -1,5 +1,7 @@
 package com.recursion;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 		
@@ -7,7 +9,11 @@ public class Main {
 //		print1ToN(9);
 		int a= sum(10);
 		int b= findFactorial(5);
-		System.out.println(b);
+		int[] arr = {2,4,6,9,6};
+		reverseArray(arr, 0, arr.length);
+		
+		
+		System.out.println(Arrays.toString(arr));
 		
 	}
 	public static void printNameNTimes(String name,int n) {
@@ -40,5 +46,17 @@ public class Main {
 		return n* findFactorial(n-1);
 	}
 	
-	
+	public static void reverseArray(int[] arr, int i, int n) {
+		if(i>= n/2) {
+			return;
+		}
+		swap(arr, i, n-i-1);
+		reverseArray(arr, i+1, n);
+	}
+	private static void swap(int[] arr, int i, int j) {
+		int temp= arr[i];
+		arr[i]= arr[j];
+		arr[j]= temp;
+		
+	}
 }
